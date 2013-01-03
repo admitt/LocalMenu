2479a66fbfa9b6e3d99a1ce44b9dba36c482704d
Anton Dmitrijev <anton.dmitrijev@elisa.ee>
2013-01-03 11:08:35 +0200

    Remove menu and news html. Move template under template directory.

--- js/show.js
+++ js/show.js
@@ -5,7 +5,7 @@ function news() {
 
 function menu() {
     var body = $('body');
-    $.get('menu_template.html', function(template) {
+    $.get('template/menu_template.html', function(template) {
         body.append(template);
     }, "html");
     body.attr("style", "background-color: #C4C4C4");
