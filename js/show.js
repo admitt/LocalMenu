6f52df4fbeedb5c83859bb46e73a0416af34d2a8
Anton Dmitrijev <anton.dmitrijev@elisa.ee>
2013-01-03 11:06:32 +0200

    Show either menu or news.

---
+++ js/show.js
@@ -0,0 +1,12 @@
+function news() {
+    $("body").html('<div id="newsFeed"></div>');
+    new News();
+}
+
+function menu() {
+    var body = $('body');
+    $.get('menu_template.html', function(template) {
+        body.append(template);
+    }, "html");
+    body.attr("style", "background-color: #C4C4C4");
+}
